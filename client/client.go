cc8a87141380ffa6179e15fae4241b06aab4ee71
luishsr <luishsoares@gmail.com>
2023-04-26 13:00:27 -0300

    Fix mod files

--- client/client.go
+++ client/client.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/luishsr/tcpconnector"
 )
 
 func main() {
 	fmt.Println("main() method")
+
+	//calls tcpconnector method
+	response := tcpconnector.Connect()
+
+	fmt.Println(response)
 }
--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module github.com/luishsr/client
 
 go 1.19
+
+require github.com/luishsr/tcpconnector v0.0.0-20230426150848-004a9384ad94 // indirect
